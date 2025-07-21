a7111677527753a93b9664268acee5ec0b5b7b55
Steffen <steffen.begemann@stud.uni-hannover.de>
2025-07-21 13:59:37 +0000

    do some automatisation for the benchmarks

---
+++ experiments/process-results.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+import csv
+import json
+import os
+
+workloads = ["a", "b"]
+
+fieldnames = ["service", "workload", "load/run", "metric", "measurement", "value"]
+data = []
+
+for s in ["baseline", "dma_copy"]:
+    for workload in workloads:
+        for op in ["load", "run"]:
+            filename = f"./{s}/{op}_workload{workload}.json"
+            if not os.path.exists(filename):
+                print(f"File {filename} does not exist, skipping...")
+                continue
+            
+            with open(filename, "r") as f:
+                json_data = json.load(f)
+                for entry in json_data:
+                    current = [s, workload, op, entry["metric"], entry["measurement"], entry["value"]]
+                    if entry["metric"] == "OVERALL" and "Throughput" in entry["measurement"]:
+                        print(current)
+                    data.append(current)
+
+
+
+with open('experiment-results.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter=';')
+    writer.writerow(fieldnames)
+    writer.writerows(data)
---
+++ run-experiment.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Cleanup
+rm -rf experiments/baseline
+rm -rf experiments/dma_copy
+
+echo "Please start the baseline experiment on the server and press Enter to continue..."
+read -r
+
+# check if ../experiments/baseline exists
+if [ ! -d "experiments/baseline" ]; then
+  echo "Creating directory experiments/baseline"
+  mkdir -p experiments/baseline
+fi
+
+# loop over all workloads
+for workload in a b; do
+    # Load data
+    python3 bin/ycsb load thesis -threads 5 -P workloads/thesis_workload${workload} -p thesis.ip=http://192.168.100.203:8080
+    cp experiments/workload${workload}.json experiments/baseline/load_workload${workload}.json
+
+    # Run experiment
+    python3 bin/ycsb run thesis -threads 5 -P workloads/thesis_workload${workload} -p thesis.ip=http://192.168.100.203:8080
+    cp experiments/workload${workload}.json experiments/baseline/run_workload${workload}.json
+
+    echo "Please reset the database and press Enter to continue..."
+    read -r
+done
+
+
+
+echo "Now start dma copy on the server and press Enter to continue..."
+read -r
+
+# check if ../experiments/dma_copy exists
+if [ ! -d "experiments/dma_copy" ]; then
+  echo "Creating directory experiments/dma_copy"
+  mkdir -p experiments/dma_copy
+fi
+
+for workload in a b; do
+    # Load data
+    python3 bin/ycsb load thesis -threads 5 -P workloads/thesis_workload${workload} -p thesis.ip=http://192.168.100.203:8080
+    cp experiments/workload${workload}.json experiments/dma_copy/load_workload${workload}.json
+
+    # Run experiment
+    python3 bin/ycsb run thesis -threads 5 -P workloads/thesis_workload${workload} -p thesis.ip=http://192.168.100.203:8080
+    cp experiments/workload${workload}.json experiments/dma_copy/run_workload${workload}.json
+
+    echo "Please reset the database and press Enter to continue..."
+    read -r
+done
